4fb011aa00f990d647edafffed29397e772c4e82
borenho <borecollins@gmail.com>
2017-06-13 01:59:58 +0300

    Test if n is an integer

---
+++ check_prime.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    """ Checks for prime nos within a range """
+
+    primes = []
+    for num in range(0, n + 1):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                primes.append(num)
+    
+    return primes
--- test_is_prime.py
+++ test_is_prime.py
@@ -6,3 +6,11 @@ from check_prime import *
 class CheckPrimeTestCase(unittest.TestCase):
     def test_first_four_primes_included(self):
         self.assertEqual([2, 3, 5, 7], is_prime(10))
+
+    def test_non_primes_not_included(self):
+        self.assertNotIn(4, is_prime(10))
+        self.assertNotIn(9, is_prime(10))
+
+    def test_n_is_integer(self):
+        with self.assertRaises(TypeError):
+            is_prime('10')
